bff06340edc4e056445ecea7af842562f32f138f
Britt Shook <brittanysshook@gmail.com>
2023-11-28 14:15:21 -0500

    upload week 1-5 assignments

---
+++ typescript/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+let sale = 342324;
+let course = 'TypeScript';
+let is_published = true;
+function render(document) {
+    console.log(document);
+}
+let numbers = [1, 2, 3];
+const user = ['Britt', 25];
+//# sourceMappingURL=index.js.map
---
+++ typescript/src/index.ts
@@ -0,0 +1,12 @@
+let sale: number = 342_324;
+let course: string = 'TypeScript';
+let is_published: boolean = true;
+
+function render(document: any) {
+    console.log(document);
+}
+
+let numbers: number[] = [1, 2, 3];
+
+const user: [string, number] = ['Britt', 25];
+
